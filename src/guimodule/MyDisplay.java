b26eef89fdb501f4844da79e2e5d997ea0937bd0
chuan1900 <shiyican@hotmail.com>
2017-03-12 13:03:29 -0400

    share projectof UCSDUnfoldingMap

---
+++ src/guimodule/MyDisplay.java
@@ -0,0 +1,17 @@
+package guimodule;
+
+import processing.core.PApplet;
+
+public class MyDisplay extends PApplet{
+	
+	public void setup(){
+		size(400, 400);
+		background(0, 0, 0);
+		
+	}
+	
+	public void draw(){
+		
+	}
+
+}
